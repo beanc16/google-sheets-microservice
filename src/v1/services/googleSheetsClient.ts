089ba56b110d7d86326239dae866d7190a9dcd6a
beanc16 <bean.christopher@outlook.com>
2024-10-20 19:12:52 -0400

    Make use of public and private on google sheets client class

--- src/v1/services/googleSheetsClient.ts
+++ src/v1/services/googleSheetsClient.ts
@@ -4,18 +4,18 @@ export type GetRangesParameters = sheets_v4.Params$Resource$Spreadsheets$Values$
 
 export class GoogleSheetsClient
 {
-    static #client: sheets_v4.Sheets;
+    private static client: sheets_v4.Sheets;
 
-    static #parseCredentials(): object
+    private static parseCredentials(): object
     {
         const stringifiedCredentials = Buffer.from(process.env.GOOGLE_SHEETS_KEYFILE_BASE64_ENCODED ?? '', 'base64').toString();
         return JSON.parse(stringifiedCredentials) as object;
     }
 
-    static #initialize(): sheets_v4.Sheets
+    private static initialize(): sheets_v4.Sheets
     {
-        if (!this.#client) {
-            const credentials = this.#parseCredentials();
+        if (!this.client) {
+            const credentials = this.parseCredentials();
 
             const googleAuthClient = new auth.GoogleAuth({
                 credentials,
@@ -24,68 +24,68 @@ export class GoogleSheetsClient
                 ],
             });
 
-            this.#client = sheets({
+            this.client = sheets({
                 version: 'v4',
                 auth: googleAuthClient,
             });
         }
 
-        return this.#client;
+        return this.client;
     }
 
-    static async getRange(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Get): Promise<sheets_v4.Schema$ValueRange>
+    public static async getRange(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Get): Promise<sheets_v4.Schema$ValueRange>
     {
-        this.#initialize();
+        this.initialize();
 
-        const rows = await this.#client.spreadsheets.values.get(parameters);
+        const rows = await this.client.spreadsheets.values.get(parameters);
         return rows.data;
     }
 
-    static async getRanges(parameters: GetRangesParameters): Promise<sheets_v4.Schema$BatchGetValuesResponse>
+    public static async getRanges(parameters: GetRangesParameters): Promise<sheets_v4.Schema$BatchGetValuesResponse>
     {
-        this.#initialize();
+        this.initialize();
 
-        const rows = await this.#client.spreadsheets.values.batchGet(parameters);
+        const rows = await this.client.spreadsheets.values.batchGet(parameters);
         return rows.data;
     }
 
-    static async update(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Update): Promise<sheets_v4.Schema$UpdateValuesResponse>
+    public static async update(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Update): Promise<sheets_v4.Schema$UpdateValuesResponse>
     {
-        this.#initialize();
+        this.initialize();
 
-        const rows = await this.#client.spreadsheets.values.update(parameters);
+        const rows = await this.client.spreadsheets.values.update(parameters);
         return rows.data;
     }
 
-    static async batchUpdate(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Batchupdate): Promise<sheets_v4.Schema$BatchUpdateValuesResponse>
+    public static async batchUpdate(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Batchupdate): Promise<sheets_v4.Schema$BatchUpdateValuesResponse>
     {
-        this.#initialize();
+        this.initialize();
 
-        const rows = await this.#client.spreadsheets.values.batchUpdate(parameters);
+        const rows = await this.client.spreadsheets.values.batchUpdate(parameters);
         return rows.data;
     }
 
-    static async clear(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Clear): Promise<sheets_v4.Schema$ClearValuesResponse>
+    public static async clear(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Clear): Promise<sheets_v4.Schema$ClearValuesResponse>
     {
-        this.#initialize();
+        this.initialize();
 
-        const rows = await this.#client.spreadsheets.values.clear(parameters);
+        const rows = await this.client.spreadsheets.values.clear(parameters);
         return rows.data;
     }
 
-    static async batchClear(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Batchclear): Promise<sheets_v4.Schema$BatchClearValuesResponse>
+    public static async batchClear(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Batchclear): Promise<sheets_v4.Schema$BatchClearValuesResponse>
     {
-        this.#initialize();
+        this.initialize();
 
-        const rows = await this.#client.spreadsheets.values.batchClear(parameters);
+        const rows = await this.client.spreadsheets.values.batchClear(parameters);
         return rows.data;
     }
 
-    static async append(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Append): Promise<sheets_v4.Schema$AppendValuesResponse>
+    public static async append(parameters: sheets_v4.Params$Resource$Spreadsheets$Values$Append): Promise<sheets_v4.Schema$AppendValuesResponse>
     {
-        this.#initialize();
+        this.initialize();
 
-        const rows = await this.#client.spreadsheets.values.append(parameters);
+        const rows = await this.client.spreadsheets.values.append(parameters);
         return rows.data;
     }
 }
